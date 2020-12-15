2f44aa77e0f0c6b47b529418ea14b8f9df273edb
CloudNiner <andy@finkmiller.com>
2020-12-15 14:50:33 -0500

    Add TypeScript support
    Via manual types provided by index.d.ts.
    
    See discussion in https://github.com/azavea/react-showtime/pull/10
    for the alternatives discussed.
    
    The types are compiled in CI, which should
    catch at least some potential issues with
    API changes.
    
    A TS Example is provided at src/TypeScriptExample.tsx
    
    For now it's part of the library source
    tree -- we only use it to verify the types
    compile.
    
    Future work may include a TypeScript example
    on the public docs site.

---
+++ index.d.ts
@@ -0,0 +1,47 @@
+import { MutableRefObject, ReactElement, Ref } from "react";
+
+import * as CSS from "csstype";
+
+export as namespace Showtime;
+
+export type PredefinedTransition = "slideFade" | "slide" | "fade" | "rise" | "scale"
+
+export interface TransitionProperties {
+    value: string;
+    duration?: CSS.Property.TransitionDuration;
+    delay?: CSS.Property.TransitionDelay;
+    easing?: CSS.Property.TransitionTimingFunction;
+}
+
+export type Transition =
+    PredefinedTransition |
+    { [key: string]: string | number | TransitionProperties };
+
+export interface ShowtimeSettings {
+    startHidden?: boolean;
+    startWithTransition?: boolean;
+    transition?: Transition;
+    showTransition?: Transition;
+    hideTransition?: Transition;
+    duration?: CSS.Property.TransitionDuration;
+    delay?: CSS.Property.TransitionDelay;
+    easing?: CSS.Property.TransitionTimingFunction;
+    showDuration?: CSS.Property.TransitionDuration;
+    showDelay?: CSS.Property.TransitionDelay;
+    showEasing?: CSS.Property.TransitionTimingFunction;
+    hideDuration?: CSS.Property.TransitionDuration;
+    hideDelay?: CSS.Property.TransitionDelay;
+    hideEasing?: CSS.Property.TransitionTimingFunction;
+}
+
+export interface ShowtimeProps<T> extends ShowtimeSettings {
+    children: JSX.Element | ((ref: Ref<T>) => JSX.Element);
+    show: boolean;
+    onHidden?: () => void;
+    onShowing?: () => void;
+}
+
+export function useShowtime<T>(settings: PredefinedTransition | ShowtimeSettings):
+    [MutableRefObject<T>, boolean, () => void, () => void];
+
+export function Showtime<T>(props: ShowtimeProps<T>): ReactElement<ShowtimeProps<T>>;
