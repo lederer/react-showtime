f9ef13c41f3e1e51d08c07c217c19012863bf761
Scott Lederer <scottlederer@gmail.com>
2023-12-14 14:31:10 -0500

    Replace rollup with vite

---
+++ vite.config.js
@@ -0,0 +1,16 @@
+import { resolve } from "path";
+import { defineConfig } from "vite";
+import eslint from "@rollup/plugin-eslint";
+
+export default defineConfig({
+    build: {
+        lib: {
+            entry: resolve(__dirname, "src/index.js"),
+            name: "ReactShowtime",
+        },
+        rollupOptions: {
+            external: ["react", "react-dom"],
+            plugins: [eslint()],
+        },
+    },
+});
